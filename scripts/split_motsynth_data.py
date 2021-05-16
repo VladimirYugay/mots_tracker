c5ee6809ab9ee2443c0f09a9808b0c8120d33e57
VladimirYugay <voviktyl@gmail.com>
2021-05-16 22:28:05 +0200

    Add new split strategy

--- scripts/split_motsynth_data.py
+++ scripts/split_motsynth_data.py
@@ -36,69 +36,81 @@ _logger = logging.getLogger(__name__)
     "--p",
     "--proportions",
     "proportions",
-    type=(float, float, float),
-    default=(0.6, 0.2, 0.2),
-    help="Proportions to split the dataset to train/val/test (should sum up to 1)",
+    type=(float, float),
+    default=(0.8, 0.2),
+    help="Proportions to split the dataset to train/val (should sum up to 1)",
+)
+@click.option(
+    "--ts",
+    "--test_seqs_path",
+    "test_seqs_path",
+    default="/home/vy/university/thesis/datasets/MOTSynth",
+    type=click.Path(exists=True),
+    help="File with sequences reserved for test",
+)
+@click.option(
+    "--only_dynamic",
+    "only_dynamic",
+    help="Use only dynamic sequences",
+    flag_value=True,
 )
 @click.version_option(mots_tracker.__version__)
-def main(data_dir, output_dir, proportions):
+def main(data_dir, output_dir, proportions, test_seqs_path, only_dynamic):
     data_dir = Path(data_dir)
     print("Splitting sequences in proportions: {}".format(proportions))
     np.random.seed(42)
+
+    test_seqs = set(np.load(test_seqs_path))
     static_seq_ids, dynamic_seq_ids = [], []
     parser = ConfigParser()
     for seq_path in sorted((data_dir / "all").glob("*"), key=lambda p: str(p)):
         parser.read(str(seq_path / "seqinfo.ini"), encoding=None)
         seq_name = parser.get("Sequence", "name")
         dynamic = parser.getint("Sequence", "dynamic")
+        if seq_name in test_seqs:
+            continue
         if dynamic:
             dynamic_seq_ids.append(seq_name)
-        else:
+        elif not only_dynamic:
             static_seq_ids.append(seq_name)
     print("Static sequences: {}".format(len(static_seq_ids)))
     print("Dynamic sequences: {}".format(len(dynamic_seq_ids)))
     static_seq_ids = np.array(static_seq_ids)
     dynamic_seq_ids = np.array(dynamic_seq_ids)
 
-    def split_data(data, p=(0.6, 0.2, 0.2)):
+    def split_data(data, p=(0.8, 0.2)):
         """ Splits data in train, val"""
         np.random.shuffle(data)
         n = data.shape[0]
-        vstart = int((p[0]) * n)
-        tstart = int((p[0] + p[1]) * n)
-        return np.split(data, [vstart, tstart])
+        return data[:, int(n * p[0])], data[int(n * p[0]) :]
 
-    st_train, st_val, st_test = split_data(static_seq_ids, proportions)
+    st_train, st_val = split_data(static_seq_ids, proportions)
     print(
-        "Static split is train: {}, validation: {}, test: {}".format(
-            st_train.shape[0], st_val.shape[0], st_test.shape[0]
+        "Static split is train: {}, validation: {}".format(
+            st_train.shape[0], st_val.shape[0]
         )
     )
-    d_train, d_val, d_test = split_data(dynamic_seq_ids, proportions)
+    d_train, d_val = split_data(dynamic_seq_ids, proportions)
     print(
-        "Dynamic split is train: {}, validation: {}, test: {}".format(
-            d_train.shape[0], d_val.shape[0], d_test.shape[0]
+        "Dynamic split is train: {}, validation: {}".format(
+            d_train.shape[0], d_val.shape[0]
         )
     )
 
     train_ids = np.concatenate((st_train, d_train))
     val_ids = np.concatenate((st_val, d_val))
-    test_ids = np.concatenate((st_test, d_test))
 
     assert not set(train_ids) & set(val_ids)
-    assert not set(train_ids) & set(test_ids)
-    assert not set(val_ids) & set(test_ids)
 
     print(
         "Overall split is train: {}, validation: {}, test: {}".format(
-            train_ids.shape[0], val_ids.shape[0], test_ids.shape[0]
+            train_ids.shape[0], val_ids.shape[0], len(test_seqs)
         )
     )
     output_path = Path(output_dir) / "split_{}_{}_{}".format(*proportions)
     output_path.mkdir(parents=True, exist_ok=True)
     np.savetxt(str(output_path / "train.txt"), train_ids, fmt="%s")
     np.savetxt(str(output_path / "val.txt"), val_ids, fmt="%s")
-    np.savetxt(str(output_path / "test.txt"), test_ids, fmt="%s")
 
 
 if __name__ == "__main__":
