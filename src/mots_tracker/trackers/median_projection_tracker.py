42e7227b5719576e53c9d65728ceb9632a99b937
VladimirYugay <voviktyl@gmail.com>
2021-05-19 22:04:27 +0200

    First implementation of reprojection tracker

--- src/mots_tracker/trackers/median_projection_tracker.py
+++ src/mots_tracker/trackers/median_projection_tracker.py
@@ -1,10 +1,9 @@
-""" Class tracking medians based on projected IoU """
+""" Class tracking medians """
 import numpy as np
-import open3d as o3d
 
 from mots_tracker import utils
-from mots_tracker.kalman_filters import MedianKalmanFilter
-from mots_tracker.trackers import BaseTracker
+from mots_tracker.kalman_filters.median_kalman_filter import MedianKalmanFilter
+from mots_tracker.trackers.base_tracker import BaseTracker
 from mots_tracker.trackers.tracker_helpers import (
     depth_median_filter,
     iou_masks,
@@ -13,131 +12,84 @@ from mots_tracker.trackers.tracker_helpers import (
 
 
 class MedianProjectionTracker(BaseTracker):
-    def __init__(self, max_age=1, min_hits=3, dist_threshold=0.3):
+    def __init__(self, max_age=1, min_hits=3, dist_threshold=0.3, dims=(1080, 1920)):
         BaseTracker.__init__(
             self, max_age=max_age, min_hits=min_hits, dist_threshold=dist_threshold
         )
         self.representation_size = 3  # x, y, z coordinates of a median
-        self.projections = np.empty((0, 3))
-        self.accumulated_egomotion = np.identity(4)
+        self.dims = dims
 
     def update(self, sample, intrinsics):
         """ updates the state of the tracker """
-        detections, info = self.compute_detections(
+        import time
+
+        start = time.time()
+        detections, detection_projections, info = self.compute_detections(
             sample, intrinsics
-        )  # (medians, masks, egomotion, intrinsic), info
-        self.frame_count += 1
-        trackers_predictions = self.predict()  # (medians, velocities, clouds)
-        matched, unmatched_detections, _ = self.associate_detections_to_trackers(
-            detections, trackers_predictions
         )
-        medians, _, _, _ = detections
-        self.update_matched_trackers(matched, medians, info)
-        self.init_unmatched_trackers(unmatched_detections, medians, info)
+        print(time.time() - start)
+        self.frame_count += 1
+
+        start = time.time()
+        trackers_predictions, trackers_masks = self.predict()
+        print(time.time() - start)
+        start = time.time()
+        matched, unmatched_detections, _ = self.associate_detections_to_trackers(
+            (detections, detection_projections), (trackers_predictions, trackers_masks)
+        )
+        print(time.time() - start)
+        self.update_matched_trackers(matched, detections, info)
+        self.init_unmatched_trackers(unmatched_detections, detections, info)
         return self.filter_trackers()
 
-    def associate_detections_to_trackers(self, detections, trackers_predictions):
-        """ Assigns detections to tracked object """
-        if len(trackers_predictions[0]) == 0:
-            return (
-                np.empty((0, 2), dtype=int),
-                np.arange(len(detections[0])),
-                np.empty((0, self.representation_size)),
-            )
-
-        # detections are (medians + vel, clouds)
-        detection_medians, detection_masks, egomotion, intrinsic = detections
-        tracker_medians, tracker_velocities, tracker_clouds = trackers_predictions
-
-        # rotate, translate, project tracker point clouds
-        dimensions = detection_masks[0].shape
-        # accumulate egomotion
-        self.accumulated_egomotion = self.accumulated_egomotion.dot(
-            np.linalg.inv(egomotion)
+    def compute_detections(self, sample, intrinsics):
+        """ computes representations for the point clouds as medians """
+        clouds = utils.compute_mask_clouds_no_color(
+            sample["depth"],
+            sample["masks"],
+            sample["intrinsics"],
+            depth_median_filter,
         )
-        transformed_tracker_clouds = [
-            o3d.geometry.PointCloud(cloud) for cloud in tracker_clouds
-        ]
-        for cloud, velocity in zip(transformed_tracker_clouds, tracker_velocities):
-            cloud.translate(velocity)
-        for cloud in transformed_tracker_clouds:
-            cloud.transform(self.accumulated_egomotion)
+        medians = np.array(
+            [np.median(np.asarray(cld.points), axis=0) for cld in clouds]
+        )
+        for cloud in clouds:
+            cloud.transform(sample["egomotion"])
         projections = np.array(
             [
-                utils.cloud2img(cloud, dimensions, intrinsic)
-                for cloud in transformed_tracker_clouds
+                utils.cloud2img(cloud, self.dims, sample["intrinsics"])
+                for cloud in clouds
             ]
         )
-        self.projections = projections
-
-        iou_matrix = iou_masks(detection_masks, projections)
-
-        if min(iou_matrix.shape) > 0:
-            a = (iou_matrix > self.dist_threshold).astype(np.int32)
-            if a.sum(1).max() == 1 and a.sum(0).max() == 1:
-                matched_indices = np.stack(np.where(a), axis=1)
-            else:
-                matched_indices = linear_assignment(-iou_matrix)
-        else:
-            matched_indices = np.empty(shape=(0, 2))
-        unmatched_detections = []
-        for d, _ in enumerate(detection_medians):
-            if d not in matched_indices[:, 0]:
-                unmatched_detections.append(d)
-        unmatched_trackers = []
-        for t, _ in enumerate(tracker_medians):
-            if t not in matched_indices[:, 1]:
-                unmatched_trackers.append(t)
-        # filter out matched with low IOU
-        matches = []
-        for m in matched_indices:
-            if iou_matrix[m[0], m[1]] < self.dist_threshold:
-                unmatched_detections.append(m[0])
-                unmatched_trackers.append(m[1])
-            else:
-                matches.append(m.reshape(1, 2))
-        if len(matches) == 0:
-            matches = np.empty((0, 2), dtype=int)
-        else:
-            matches = np.concatenate(matches, axis=0)
-        return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+        if medians.shape[0] == 0:
+            medians = (
+                np.empty((0, self.representation_size)),
+                np.empty((0, *sample["image"])),
+                {},
+            )
+        info = {
+            i: {
+                "mask": sample["masks"][i, :],
+                "raw_mask": sample["raw_masks"][i],
+                "box": sample["boxes"][i],
+            }
+            for i in range(medians.shape[0])
+        }
+        return medians, projections, info
 
     def predict(self):
         """ gets predictions from current trackers """
-        predictions = np.zeros(
-            (len(self.trackers), 2 * self.representation_size)
-        )  # append velocity
+        predictions = np.zeros((len(self.trackers), self.representation_size))
         to_del = []
         for t, prediction in enumerate(predictions):
-            self.trackers[t].predict()
-            prediction[:] = np.array(
-                self.trackers[t].history[-1][:, 0]
-            )  # get the most recent median and velocity
+            prediction[:] = np.array(self.trackers[t].predict())
             if np.any(np.isnan(prediction)):
                 to_del.append(t)
         predictions = np.ma.compress_rows(np.ma.masked_invalid(predictions))
         for t in reversed(to_del):
             self.trackers.pop(t)
-        medians, velocities = predictions[:, :3], predictions[:, 3:]
-        clouds = [tracker.info["cloud"] for tracker in self.trackers]
-        return medians, velocities, clouds
-
-    def compute_detections(self, sample, intrinsics):
-        """ computes representations for the point clouds as medians """
-        clouds = utils.compute_mask_clouds(sample, depth_median_filter)
-        medians = np.array(
-            [np.median(np.asarray(cld.points), axis=0) for cld in clouds]
-        )
-        info = {
-            i: {"box": sample["boxes"][i], "cloud": clouds[i]}
-            for i in range(len(clouds))
-        }
-        return (
-            medians,
-            sample["masks"],
-            sample["egomotion"],
-            sample["intrinsics"],
-        ), info
+        masks = np.array([t.info["mask"] for t in self.trackers])
+        return predictions, masks
 
     def init_unmatched_trackers(self, unmatched_detections_ids, detections, info):
         for i in unmatched_detections_ids:
@@ -150,8 +102,53 @@ class MedianProjectionTracker(BaseTracker):
             if (trk.time_since_update < 1) and (
                 trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits
             ):
-                ret.append((None, None, trk.info["box"], trk.id + 1))
+                ret.append(
+                    (trk.kf.x[:3][:, 0], trk.info["mask"], trk.info["box"], trk.id + 1)
+                )
             i -= 1
             if trk.time_since_update > self.max_age:
                 self.trackers.pop(i)
         return ret
+
+    def associate_detections_to_trackers(self, detections, trackers):
+        """ Assigns detections to tracked object (both as bounding boxes) """
+        detections, detections_projections = detections
+        trackers, trackers_masks = trackers
+        if len(trackers) == 0:
+            return (
+                np.empty((0, 2), dtype=int),
+                np.arange(len(detections)),
+                np.empty((0, self.representation_size)),
+            )
+
+        iou_projections = iou_masks(detections_projections, trackers_masks)
+
+        if min(iou_projections.shape) > 0:
+            a = (iou_projections > self.dist_threshold).astype(np.int32)
+            if a.sum(1).max() == 1 and a.sum(0).max() == 1:
+                matched_indices = np.stack(np.where(a), axis=1)
+            else:
+                matched_indices = linear_assignment(-iou_projections)
+        else:
+            matched_indices = np.empty(shape=(0, 2))
+        unmatched_detections = []
+        for d, det in enumerate(detections):
+            if d not in matched_indices[:, 0]:
+                unmatched_detections.append(d)
+        unmatched_trackers = []
+        for t, trk in enumerate(trackers):
+            if t not in matched_indices[:, 1]:
+                unmatched_trackers.append(t)
+        # filter out matched with low IOU
+        matches = []
+        for m in matched_indices:
+            if iou_projections[m[0], m[1]] < self.dist_threshold:
+                unmatched_detections.append(m[0])
+                unmatched_trackers.append(m[1])
+            else:
+                matches.append(m.reshape(1, 2))
+        if len(matches) == 0:
+            matches = np.empty((0, 2), dtype=int)
+        else:
+            matches = np.concatenate(matches, axis=0)
+        return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
