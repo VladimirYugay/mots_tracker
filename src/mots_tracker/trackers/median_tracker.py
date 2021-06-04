8f551b8a6ef6c62dc6275daa2c15907034a6bf8b
VladimirYugay <voviktyl@gmail.com>
2021-06-04 12:02:24 +0200

    Add depth filter arguments to median tracker

--- src/mots_tracker/trackers/median_tracker.py
+++ src/mots_tracker/trackers/median_tracker.py
@@ -1,4 +1,6 @@
 """ Class tracking medians """
+from functools import partial
+
 import numpy as np
 
 from mots_tracker import utils
@@ -12,18 +14,27 @@ from mots_tracker.trackers.tracker_helpers import (
 
 
 class MedianTracker(BaseTracker):
-    def __init__(self, max_age=1, min_hits=3, dist_threshold=0.3, use_egomotion=False):
+    def __init__(
+        self,
+        max_age=1,
+        min_hits=3,
+        dist_threshold=0.3,
+        use_egomotion=False,
+        depth_deviation=0.3,
+    ):
         BaseTracker.__init__(
             self, max_age=max_age, min_hits=min_hits, dist_threshold=dist_threshold
         )
         self.representation_size = 3  # x, y, z coordinates of a median
         self.use_egomotion = use_egomotion
         self.accumulated_egomotion = np.identity(4)
+        self.depth_deviation = depth_deviation
 
     def compute_detections(self, sample, intrinsics):
         """ computes representations for the point clouds as medians """
+        cloud_filter = partial(depth_median_filter, radius=self.depth_deviation)
         clouds = utils.compute_mask_clouds_no_color(
-            sample["depth"], sample["masks"], sample["intrinsics"], depth_median_filter
+            sample["depth"], sample["masks"], sample["intrinsics"], cloud_filter
         )
         if self.use_egomotion:
             self.accumulated_egomotion = self.accumulated_egomotion.dot(
