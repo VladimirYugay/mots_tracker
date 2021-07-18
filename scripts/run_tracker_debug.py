710dc402d4f786f3a09b040768ce3ea0d143cd74
VladimirYugay <voviktyl@gmail.com>
2021-07-19 00:48:05 +0200

    Add debug running script

---
+++ scripts/run_tracker_debug.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import logging
+import sys
+from pathlib import Path
+
+import click
+import numpy as np
+
+import mots_tracker
+from mots_tracker import readers, trackers, utils
+from mots_tracker.io_utils import (
+    get_instance,
+    load_yaml,
+    print_mot_format,
+    print_mots_format,
+)
+
+_logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--cp",
+    "--config_path",
+    "config_path",
+    default="/home/vy/university/thesis/datasets/MOTSynth/",
+    type=click.Path(exists=True),
+    help="Path to the dataset: MOTS, MOTSynth, KITTI",
+)
+@click.option("-v", "--verbose", "log_level", flag_value=logging.INFO, default=True)
+@click.version_option(mots_tracker.__version__)
+def main(config_path, log_level):
+    """
+    config_path: Path to the dataset \n
+    log_level: Logging level \n
+    """
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=log_level,
+        datefmt="%Y-%m-%d %H:%M",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    config = load_yaml(config_path)
+
+    reader_config = config["reader"]["args"]
+    output_path = Path(config["output_path"])
+    output_path.mkdir(parents=True, exist_ok=True)
+    (output_path / "MOTS").mkdir(parents=True, exist_ok=True)
+    (output_path / "MOT").mkdir(parents=True, exist_ok=True)
+    reader = get_instance(readers, "reader", config)
+    seq_ids = sorted(reader.sequence_info.keys())
+
+    for seq in seq_ids:
+        tracker = get_instance(trackers, "tracker", config)
+        width = reader.sequence_info[seq]["img_width"]
+        height = reader.sequence_info[seq]["img_height"]
+        mot_out_file = open(str(output_path / "MOT" / "{}.txt".format(seq)), "w")
+        mots_out_file = open(str(output_path / "MOTS" / "{}.txt".format(seq)), "w")
+        for frame in range(reader.sequence_info[seq]["length"]):
+            logging.log(
+                log_level, "Processing sequence {}, frame {}".format(seq, frame + 1)
+            )
+            sample = reader.read_sample(seq, frame)
+            frame += 1
+
+            for box_id, bb in enumerate(sample["boxes"]):
+                bb = bb.astype(np.int32)
+
+            tracks = tracker.update(sample, sample["intrinsics"])
+
+            for (raw_mask, box, idx) in tracks:
+                if (
+                    "resize_shape" in reader_config
+                    and reader_config["resize_shape"] is not None
+                ):
+                    box = utils.resize_boxes(
+                        box[None, :], reader_config["resize_shape"], (width, height)
+                    )
+                print_mot_format(frame, idx, box, mot_out_file)
+                print_mots_format(frame, idx, height, width, raw_mask, mots_out_file)
+
+        mot_out_file.close()
+        mots_out_file.close()
+
+
+if __name__ == "__main__":
+    main()
