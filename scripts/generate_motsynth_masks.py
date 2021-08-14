2b107cdc05205fa26f0cea1a4b275a1da98346ce
VladimirYugay <voviktyl@gmail.com>
2021-08-14 10:50:18 +0500

    Fix motsynth masks generation script

--- scripts/generate_motsynth_masks.py
+++ scripts/generate_motsynth_masks.py
@@ -1,5 +1,4 @@
 """ Generates motsynth segmentation images  """
-import json
 import logging
 import sys
 from multiprocessing import Pool
@@ -7,8 +6,8 @@ from pathlib import Path
 
 import click
 
-from mots_tracker import utils
-from mots_tracker.readers import NewMOTSynthReader
+from mots_tracker import readers, utils
+from mots_tracker.io_utils import get_instance, load_yaml
 
 _logger = logging.getLogger(__name__)
 
@@ -39,30 +38,22 @@ def make_masks(reader, seq_id, output_path):
 @click.command()
 @click.option("--c", "--cores", "cores", default=4)
 @click.option(
-    "--ip",
-    "--input_path",
-    "input_path",
-    default="/home/vy/university/thesis/datasets/MOTSynth/",
-    type=click.Path(exists=True),
-    help="Path to depth motsynth annotations",
-)
-@click.option(
     "--op",
     "--output_path",
     "output_path",
     default="/home/vy/university/thesis/datasets/MOTSynth",
     type=click.Path(exists=True),
-    help="Path to resulting depth frames",
+    help="Path to resulting segmentation masks",
 )
 @click.option(
-    "--rc",
-    "--reader_config",
-    "reader_cfg_path",
-    default="./configs/reader_configs/motsynth_reader_config.json",
+    "--cp",
+    "--config_path",
+    "config_path",
+    default="./configs/median_tracker_config.yaml",
     type=click.Path(exists=True),
-    help="path to reader config file",
+    help="path to the config file",
 )
-def main(input_path, output_path, reader_cfg_path, cores):
+def main(output_path, config_path, cores):
     logging.basicConfig(
         stream=sys.stdout,
         level=logging.INFO,
@@ -72,9 +63,9 @@ def main(input_path, output_path, reader_cfg_path, cores):
     output_path = Path(output_path)
     output_path = output_path / "seg_frames"
     output_path.mkdir(parents=True, exist_ok=True)
-    with open(str(reader_cfg_path), "r") as reader_config_file:
-        reader_config = json.load(reader_config_file)
-    reader = NewMOTSynthReader(input_path, reader_config)
+
+    config = load_yaml(config_path)
+    reader = get_instance(readers, "reader", config)
     seq_ids = sorted(reader.sequence_info.keys())
     pool = Pool(cores)
     args = [(reader, seq_id, output_path) for seq_id in seq_ids]
