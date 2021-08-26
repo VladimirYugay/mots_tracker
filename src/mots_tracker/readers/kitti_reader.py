3f3df1a9dacb037d292fad048d07daa125d165eb
VladimirYugay <voviktyl@gmail.com>
2021-08-26 10:12:16 +0500

    Update kitti reader

--- src/mots_tracker/readers/kitti_reader.py
+++ src/mots_tracker/readers/kitti_reader.py
@@ -7,29 +7,31 @@ from mots_tracker import utils
 from mots_tracker.readers import reader_helpers
 from mots_tracker.readers.reader_helpers import read_file_names, read_kitti_bb_file
 
-DEFAULT_CONFIG = {
-    "read_boxes": True,
-    "resize_shape": None,
-    "depth_path": None,
-    "egomotion_path": None,
-}
-
 
 class KITTIReader(object):
-    def __init__(self, root_path, config):
-        self.config = DEFAULT_CONFIG.copy()
-        self.config.update(config)
-        self.root_path = Path(root_path)
+    def __init__(
+        self,
+        data_path,
+        read_boxes=True,
+        depth_path=None,
+        resize_shape=None,
+        egomotion_path=None,
+    ):
+        self.read_boxes = read_boxes
+        self.depth_path = depth_path
+        self.resize_shape = resize_shape
+        self.egomotion_path = egomotion_path
+        self.data_path = Path(data_path)
         self.sequence_info = self._init_sequence_info()
         self.cache = {}
 
-    def read_sample(self, seq_id, frame_id):
+    def read_sample(self, seq_id: str, frame_id: int) -> dict:
         """Reads sample
         Args:
-            seq_id (int): seqeunce id
-            frame_id (int): frame id
+            seq_id: seqeunce id
+            frame_id: frame id
         Returns:
-            dict: sample
+            sample dictionary
         """
         if seq_id not in self.cache:
             self._init_cache(seq_id)
@@ -39,30 +41,24 @@ class KITTIReader(object):
         boxes, box_ids, image, depth, egomotion = [None] * 5
         img_path = self.cache["img_names"][frame_id]
         image = utils.load_image(img_path)
-        if self.config["read_boxes"]:
+        if self.read_boxes:
             boxes, box_ids, obj_types = self._read_bb(frame_id)  # frames are 0 indexed
-        if self.config["depth_path"] is not None:
-            depth_path = reader_helpers.id2depthpath(
-                self.config["depth_path"],
-                "{:06d}".format(frame_id),
-                self.root_path,
-                seq_id,
-            )
-            depth = np.load(depth_path)
-        if self.config["resize_shape"] is not None:
+        if self.depth_path is not None:
+            depth_path = self.data_path / self.depth_path / seq_id
+            depth_path = str(depth_path) + "/{:06d}".format(frame_id) + ".npz"
+            depth = np.load(depth_path)["arr_0"]
+        if self.resize_shape is not None:
             width, height = image.size
             intrinsics = utils.scale_intrinsics(
-                intrinsics, (height, width), self.config["resize_shape"]
+                intrinsics, (height, width), self.resize_shape
             )
             if boxes is not None:
-                boxes = utils.resize_boxes(
-                    boxes, image.size, self.config["resize_shape"]
-                )
+                boxes = utils.resize_boxes(boxes, image.size, self.resize_shape)
             if image is not None:
-                image = utils.resize_img(image, self.config["resize_shape"])
+                image = utils.resize_img(image, self.resize_shape)
 
         return {
-            "image": image,
+            "image": np.array(image),
             "boxes": boxes,
             "box_ids": box_ids,
             "intrinsics": intrinsics,
@@ -74,7 +70,6 @@ class KITTIReader(object):
     def _read_bb(self, frame_id):
         """read all bounding boxes for a given frame MOTS format
         Args:
-            seq_id (str): sequence id
             frame_id (int): frame id
         Returns:
             boxes (ndarray), box_ids (ndarray), boj_types: boxes with their
@@ -89,14 +84,14 @@ class KITTIReader(object):
     def _init_cache(self, seq_id):
         """Initializes cache for a sequence
         Args:
-             seq_id (str): sequence id in 3-digit format
+             seq_id (str): sequence id in 4-digit format
         """
-        img_path = self.root_path / "image_02" / seq_id
+        img_path = self.data_path / "image_02" / seq_id
         img_names = reader_helpers.read_file_names(img_path)
         img_names.sort()
-        bb_path = self.root_path / "label_02" / seq_id
+        bb_path = self.data_path / "label_02" / seq_id
         bb_path = str(bb_path) + ".txt"
-        calib_path = self.root_path / "calib" / seq_id
+        calib_path = self.data_path / "calib" / seq_id
         calib_path = str(calib_path) + ".txt"
         self.cache = {
             seq_id: seq_id,
@@ -108,7 +103,7 @@ class KITTIReader(object):
     def _init_sequence_info(self):
         """ Provides info about each of the sequences sequence """
         sequence_info = {}
-        image_path = self.root_path / "image_02"
+        image_path = self.data_path / "image_02"
         for seq_id in image_path.iterdir():
             num_files = len(read_file_names(seq_id))
             seq_name = seq_id.parts[-1]
