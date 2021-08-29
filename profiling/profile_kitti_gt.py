097f4c8564842618bb200086aa018cca0888ae28
VladimirYugay <voviktyl@gmail.com>
2021-08-29 13:38:09 +0500

    Add profiling for kitti dataset

--- profiling/profile_kitti_gt.py
+++ profiling/profile_kitti_gt.py
@@ -1,7 +1,9 @@
+from functools import partial
 from pathlib import Path
 
-from mots_tracker import vis_utils
+from mots_tracker import utils, vis_utils
 from mots_tracker.readers import KITTIReader
+from mots_tracker.trackers.tracker_helpers import depth_median_filter
 from mots_tracker.vis_utils import plot_image_boxes
 
 
@@ -21,13 +23,32 @@ def profile_resizing(sample):
     vis_utils.plot_image_boxes(sample["image"], sample["boxes"], sample["box_ids"])
 
 
+def profile_clouds(reader, seq_id, frame_id):
+    """ See the clouds """
+    sample = reader.read_sample(seq_id, frame_id)
+    cloud_filter = partial(depth_median_filter, radius=0.1)
+    clouds = utils.compute_mask_clouds(sample, cloud_filter)
+    vis_utils.plot_ptcloud(clouds, False)
+
+
+def profile_scene(reader, seq_id, frame_id):
+    """ Shows pedestrians point clouds """
+    sample = reader.read_sample(seq_id, frame_id)
+    scene_cloud = utils.rgbd2ptcloud(
+        sample["image"], sample["depth"], sample["intrinsics"]
+    )
+    vis_utils.plot_ptcloud(scene_cloud)
+
+
 if __name__ == "__main__":
     root_path = Path("/home/vy/university/thesis/datasets/KITTI")
-    config = {"depth_path": "kitti"}
-    reader = KITTIReader(root_path / "training", config)
-    seq_id, frame_id = "0015", 100
+    reader = KITTIReader(
+        "/home/vy/university/thesis/datasets/KITTI/training",
+        read_boxes=True,
+        depth_path="adabins_for_mots_best",
+    )
+    seq_id, frame_id = "0007", 0
     sample = reader.read_sample(seq_id, frame_id)
-    profile_resizing(sample)
-    # profile_depth(sample)
-    # profile_boxes(sample)
-    # print(reader.sequence_info)
+    profile_depth(sample)
+    profile_scene(reader, seq_id, frame_id)
+    profile_boxes(sample)
